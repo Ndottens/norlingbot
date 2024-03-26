2734819162580edf731528226ec855667f02febb
Ndottens <ndottens@hotmail.com>
2024-03-26 15:03:49 +0100

    changed threadjs

--- src/helpers/posts/threads.js
+++ src/helpers/posts/threads.js
@@ -1,4 +1,19 @@
 module.exports = Object.freeze({
     mosquito_lake: process.env.MOSQUTIO_LAKE_THREAD_ID,
-    winding_rivulet: process.env.WINDING_RIVULET_THREAD_ID
+    winding_rivulet: process.env.WINDING_RIVULET_THREAD_ID,
+    old_burg_lake: process.env.OLD_BURG_LAKE_THREAD_ID,
+    belaya_river: process.env.BELAYA_RIVER_THREAD_ID,
+    kuori_lake: process.env.KUORI_LAKE_THREAD_ID,
+    bear_lake: process.env.BEAR_LAKE_THREAD_ID,
+    volkhov_river: process.env.VOLKHOV_RIVER_THREAD_ID,
+    severtsky_donets_river: process.env.SEVERTSKY_DONETS_RIVER_THREAD_ID,
+    sura_river: process.env.SURA_RIVER_THREAD_ID,
+    ladoga_lake: process.env.LADOGA_LAKE_THREAD_ID,
+    amber_lake: process.env.AMBER_LAKE_THREAD_ID,
+    ladoga_archipelago: process.env.LADOGA_ARCHIPELAGO_THREAD_ID,
+    akthuba_river: process.env.AKTHUBA_RIVER_THREAD_ID,
+    lower_tunguska_river: process.env.LOWER_TUNGUSKA_RIVER_THREAD_ID,
+    yama_river: process.env.YAMA_RIVER_THREAD_ID,
+    norwegian_sea: process.env.NORWEGIAN_SEA_THREAD_ID
+
 })
